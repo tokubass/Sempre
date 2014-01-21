ceb3cfab8e127f5c256d20de68497eb4614223ba
papix <mail@papix.net>
2014-01-22 05:19:29 +0900

    initial commit

---
+++ lib/Sempre.pm
@@ -0,0 +1,39 @@
+package Sempre;
+use 5.008005;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Sempre - It's new $module
+
+=head1 SYNOPSIS
+
+    use Sempre;
+
+=head1 DESCRIPTION
+
+Sempre is ...
+
+=head1 LICENSE
+
+Copyright (C) papix.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+papix E<lt>mail@papix.netE<gt>
+
+=cut
+
